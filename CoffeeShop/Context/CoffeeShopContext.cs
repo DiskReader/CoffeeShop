c34e442d14588c7f6e4178597d7f4adb1f50e073
betacaptcha <betacaptcha@outlook.com>
2022-07-05 18:11:20 +0300

    fixed CoffeeShopContext

--- CoffeeShop/Context/CoffeeShopContext.cs
+++ CoffeeShop/Context/CoffeeShopContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeShop.Context
 {
@@ -16,13 +13,12 @@ namespace CoffeeShop.Context
         {
         }
 
-        public virtual DbSet<Coffee> Coffees { get; set; } = null!;
+        public virtual DbSet<Coffee> Coffees { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=MA-BOII;Database=CoffeeShop;Trusted_Connection=True;");
             }
         }
