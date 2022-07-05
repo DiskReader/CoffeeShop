02de690f8062e0676dc5c520b914af2fc2f1d142
betacaptcha <betacaptcha@outlook.com>
2022-07-05 14:01:07 +0300

    implemented CoffeeController

--- CoffeeShop/Controllers/CoffeeController.cs
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -1,8 +1,32 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 
-public class Class1
+namespace CoffeeShop.Controllers
 {
-	public Class1()
-	{
-	}
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : ControllerBase
+    {
+        private static readonly string[] Names = new[]
+        {
+            "Arabica", "Robusta", "Decaf", "Espresso", "Latte", "Cappuccino", "Macchiato", "Americano"
+        };
+
+        private readonly ILogger<CoffeeController> _logger;
+
+        public CoffeeController(ILogger<CoffeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCoffee")]
+        public IEnumerable<Coffee> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Coffee
+                {
+                    Price = Random.Shared.Next(5, 20),
+                    Name = Names[Random.Shared.Next(Names.Length)]
+                })
+                .ToArray();
+        }
+    }
 }
