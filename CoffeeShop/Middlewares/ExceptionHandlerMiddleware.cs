5732bdae403b9946788d4b2dc60396e1954ecc41
betacaptcha <betacaptcha@outlook.com>
2022-07-07 16:02:56 +0300

    added custom exception middleware and GetCoffeeById method

---
+++ CoffeeShop/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+
+namespace CoffeeShop.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next.Invoke(httpContext);
+            }
+            catch (Exception exception)
+            {
+                await HandleException(httpContext, exception);
+            }
+        }
+
+        private async Task HandleException(HttpContext httpContext, Exception exception)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+            await httpContext.Response.WriteAsync(
+                $"Bad Request from the custom middleware. Error code {httpContext.Response.StatusCode}\n{exception.Message}");
+        }
+    }
+}
