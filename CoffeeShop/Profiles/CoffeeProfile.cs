91cde07e283c49adbb3b778767c4ff6eb7f77c48
betacaptcha <betacaptcha@outlook.com>
2022-07-12 13:37:42 +0300

    added CoffeeProfile

---
+++ CoffeeShop/Profiles/CoffeeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CoffeeShop.Models;
+using CoffeeShop.ViewModels;
+
+namespace CoffeeShop.Profiles
+{
+    public class CoffeeProfile : Profile
+    {
+        public CoffeeProfile()
+        {
+            CreateMap<CoffeeViewModel, Coffee>().ReverseMap();
+            CreateMap<Coffee, CoffeeEntity>().ReverseMap();
+        }
+    }
+}
