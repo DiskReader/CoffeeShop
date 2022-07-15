258b300ed7790a7b67be5fc95576283fd2f58411
betac <37914294+DiskReader@users.noreply.github.com>
2022-07-15 17:00:07 +0300

    added CoffeePackViewModel and CoffeePack model

---
+++ CoffeeShop/ViewModels/CoffeePackViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CoffeeShop.ViewModels
+{
+    public class CoffeePackViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<CoffeeViewModel> Coffees { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
