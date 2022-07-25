b0d2c565a132e0fbf8b7ba8263547fc5b9cc1dfa
betacaptcha <betacaptcha@outlook.com>
2022-07-25 15:07:40 +0300

    added generic controller

--- CoffeeShop/Controllers/CoffeeController.cs
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -8,51 +8,10 @@ namespace CoffeeShop.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CoffeeController : Controller
+    public class CoffeeController : GenericCoffeeShopController<CoffeeViewModel, Coffee>
     {
-        private readonly ICoffeeService _service;
-        private readonly IMapper _mapper;
-
-        public CoffeeController(ICoffeeService service, IMapper mapper)
-        {
-            _service = service;
-            _mapper = mapper;
-        }
-
-        [HttpGet]
-        public async Task<IEnumerable<CoffeeViewModel>> GetAllCoffeeAsync(CancellationToken cancellationToken)
-        {
-            var coffees = await _service.GetAllCoffeeAsync(cancellationToken);
-
-            return _mapper.Map<IEnumerable<CoffeeViewModel>>(coffees);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<CoffeeViewModel> GetCoffeeByIdAsync(int id, CancellationToken cancellationToken)
-        {
-            var coffee = await _service.GetCoffeeByIdAsync(id, cancellationToken);
-
-            return _mapper.Map<CoffeeViewModel>(coffee);
-        }
-
-        [HttpPost]
-        public async Task CreateCoffeeAsync([FromBody] CoffeeViewModel coffeeViewModel, CancellationToken cancellationToken)
-        {
-            var coffee = _mapper.Map<Coffee>(coffeeViewModel);
-            await _service.CreateCoffeeAsync(coffee, cancellationToken);
-        }
-
-        [HttpPut]
-        public async Task ChangeCoffeeAsync(int id, [FromBody] CoffeeViewModel coffeeViewModel, CancellationToken cancellationToken)
-        {
-            var coffee = _mapper.Map<Coffee>(coffeeViewModel);
-            await _service.ChangeCoffeeAsync(id, coffee, cancellationToken);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task DeleteCoffeeByIdAsync(int id, CancellationToken cancellationToken)
-        {
-            await _service.DeleteCoffeeByIdAsync(id, cancellationToken);
-        }
+        public CoffeeController(IGenericCoffeeShopService<Coffee> service, IMapper mapper)
+        :base(service, mapper)
+        { }
     }
 }
---
+++ CoffeeShop/Controllers/GenericCoffeeShopController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CoffeeShop.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class GenericCoffeeShopController<TViewModel, TModel> : Controller
+        where TViewModel : class
+        where TModel : class
+    {
+        private readonly IGenericCoffeeShopService<TModel> _service;
+        private readonly IMapper _mapper;
+
+        public GenericCoffeeShopController(IGenericCoffeeShopService<TModel> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<TViewModel>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var tModels = await _service.GetAllAsync(cancellationToken);
+
+            return _mapper.Map<IEnumerable<TViewModel>>(tModels);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<TViewModel> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var tModels = await _service.GetByIdAsync(id, cancellationToken);
+
+            return _mapper.Map<TViewModel>(tModels);
+        }
+
+        [HttpPost]
+        public async Task CreateAsync([FromBody] TViewModel tViewModel, CancellationToken cancellationToken)
+        {
+            var tModel = _mapper.Map<TModel>(tViewModel);
+            await _service.CreateAsync(tModel, cancellationToken);
+        }
+
+        [HttpPut]
+        public async Task ChangeAsync(int id, [FromBody] TViewModel tViewModel, CancellationToken cancellationToken)
+        {
+            var tModel = _mapper.Map<TModel>(tViewModel);
+            await _service.ChangeAsync(id, tModel, cancellationToken);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            await _service.DeleteByIdAsync(id, cancellationToken);
+        }
+    }
+}
